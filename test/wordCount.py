f2fecac660ea459e4a7a71fd584d6b8f76ce1f46
Kevin Apodaca <kevin.apodaca7@gmail.com>
2019-08-27 18:09:48 -0600

    created python file

---
+++ test/wordCount.py
@@ -0,0 +1,14 @@
+"""
+CS 4375: Theory of Operating Systems
+Lab 01: Into to Python
+Author: Kevin Apodaca
+Description: The purpose is to have a fairly simple python assignment that introduces the basic features and tools of python.
+"""
+#imports 
+import re
+import or
+import sys
+import math
+
+#class
+def readFile():
