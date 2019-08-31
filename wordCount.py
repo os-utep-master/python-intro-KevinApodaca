91c6b1f8299d7d862bc67ddc8b5c3d8ca645acf9
Kevin Apodaca <kevin.apodaca7@gmail.com>
2019-08-31 14:17:43 -0600

    cleaning up code

--- wordCount.py
+++ wordCount.py
@@ -1,3 +1,4 @@
+#! /usr/bin/env python3
 """
 CS 4375: Theory of Operating Systems
 Lab 01: Into to Python
@@ -5,18 +6,12 @@ Author: Kevin Apodaca
 Description: The purpose is to have a fairly simple python assignment that introduces the basic features and tools of python.
 """
 #imports 
-import re
-import os
+import re, os, sys, subprocess
 import string
 
 #taking text file and assigning it to string.
 str1 = open('declaration.txt', 'r').read()
- 
-# create a list of words separated at whitespaces
 wordList1 = str1.split(None)
- 
-# strip any punctuation marks and build modified word list
-
 wordList2 = []
 
 for word1 in wordList1:
@@ -27,18 +22,15 @@ for word1 in wordList1:
     else:
         word2 = word1
     wordList2.append(word2.lower()) #case-insensitive
- 
 
 word_freq = {}  #dictionary to store all words. Key is the word, value is the count of the word.
 for word2 in wordList2:
     word_freq[word2] = word_freq.get(word2, 0) + 1
+#make list of words from frequency list and sort them alphabetically using sort function.
+word_list = word_freq.keys()
+word_list.sort()
  
-# create a list of keys and sort the list
-# all words are lower case already
-keyList = word_freq.keys()
-keyList.sort()
- 
-print ("Frequency of each word in the word list (sorted):")
-for key2 in keyList:
-  print ("%-10s %d" % (key2, word_freq[key2]))
+print ("Sorted word frequency:")
+for words in word_list:
+  print ("%-10s %d" % (words, word_freq[words]))
 
