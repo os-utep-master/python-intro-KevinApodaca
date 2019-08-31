fcac0a220da19c5e264ba7f09730424a799b0087
Kevin Apodaca <kevin.apodaca7@gmail.com>
2019-08-31 13:55:13 -0600

    more accurate and faster method

--- wordCount.py
+++ wordCount.py
@@ -8,33 +8,37 @@ Description: The purpose is to have a fairly simple python assignment that intro
 import re
 import os
 import string
-from collections import Counter
 
-""" TEST METHOD 1, EDIT LATER
-#opening and reading file
-with open ('declaration.txt') as t:
-    testFile = t.read().upper()
-
-words = re.findall(r'\w+', testFile)
-capitals = [word.upper() for word in words]
-count_words = Counter(capitals)
-"""
-
-"""
-Simpler version test
-"""
-#creating dictionary
-wd_frequency = {}
-
-txt_file = open('declaration.txt', 'r') #hardcoded the text file to test, will use kwargs later for any file.
-text_string = txt_file.read().lower()  #case insensitive 
-match_pattern = re.findall(r'\b[a-z]{1,15}\b', text_string) #regular expression, finds all words in the bound 1 character to 15 characters, of alphabetical type
+#taking text file and assigning it to string.
+str1 = open('declaration.txt', 'r').read()
  
-for word in match_pattern:
-    count = wd_frequency.get(word,0)
-    wd_frequency[word] = count + 1
-     
-frequency_list = wd_frequency.keys()
+# create a list of words separated at whitespaces
+wordList1 = str1.split(None)
  
-for words in frequency_list:
-    print words, wd_frequency[words]
+# strip any punctuation marks and build modified word list
+
+wordList2 = []
+
+for word1 in wordList1:
+    # testing for any punctuation marks, and if found, will be removed.
+    lastchar = word1[-1:]
+    if lastchar in [",", ".", "!", "?", ";", "-", ".--", ":", ".- -"]:
+        word2 = word1.rstrip(lastchar)
+    else:
+        word2 = word1
+    wordList2.append(word2.lower()) #case-insensitive
+ 
+
+word_freq = {}  #dictionary to store all words. Key is the word, value is the count of the word.
+for word2 in wordList2:
+    word_freq[word2] = word_freq.get(word2, 0) + 1
+ 
+# create a list of keys and sort the list
+# all words are lower case already
+keyList = word_freq.keys()
+keyList.sort()
+ 
+print ("Frequency of each word in the word list (sorted):")
+for key2 in keyList:
+  print ("%-10s %d" % (key2, word_freq[key2]))
+
