c49364aac47aaaee0f462e9e28acb5ef3d926b13
Kevin Apodaca <kevin.apodaca7@gmail.com>
2019-08-31 19:38:43 -0600

    uploading all files

---
+++ lab1/Readme.md
@@ -0,0 +1,22 @@
+# Theory of Operating Systems Lab 1 Intro to Python :skull:
+
+## Description
+Create a python3 program which:
+- takes the name of an input file and output file
+- keeps track of the total the number of times each word occurs in the text file
+- excluding white space and punctuation
+- is case-insensitive
+- print out to the output file (overwriting if it exists) the list of words sorted in descending order with their respective totals separated by a space, one word per line
+
+## Requirements
+- python 3 must be installed for proper functionality (this was tested on version 3.7.4)
+
+## How to run
+Ensure you are in the proper directory that contains the *wordCount.py* file, then simply type the following in your terminal.
+
+`$ python3 wordCount.py <input_file.txt> <output_file.txt>`
+Where <input_file> is the text file that you want to count words for, and <output_file> is the text file that you want to be created which contains the output.
+
+## Contributions, Resources, & Thanks :loop:
+
+
---
+++ lab1/wordCount.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python3
+"""
+CS 4375: Theory of Operating Systems
+Lab 01: Into to Python
+Author: Kevin Apodaca
+Description: The purpose is to have a fairly simple python assignment that introduces the basic features and tools of python. This program will take a text file and determine the frequency of the
+words used, displaying the words (in alphabetical order) as well as the amount of times they appear.
+"""
+
+#imports 
+import re, os, sys
+
+#taking file arguments from terminal. I used this https://stackabuse.com/command-line-arguments-in-python/ resource to find out how to take arguments from the command line.
+wordFile, outputFile = sys.argv[1], sys.argv[2]
+word_freq = {}
+
+#taking text file and assigning it to string. Lowercasing everything because it is case insensitive.
+str1 = open(wordFile, 'r').read().lower()
+
+#I used these resources to learn about the formatting for regular expressions 
+#https://docs.python.org/3/library/re.html
+#https://www.guru99.com/python-regular-expressions-complete-tutorial.html
+
+formated_list = re.findall(r'\b[a-z]{1,15}\b', str1)
+
+#traversing list and adding to dictionary
+for word in formated_list:
+    counter = word_freq.get(word,0)
+    word_freq[word] = counter + 1 #keeping track of how many of each word
+
+list_of_words = sorted(word_freq.keys()) #sorting the final list based on keys (the actual words)
+
+with open(outputFile, 'w') as out_file:
+    for words in list_of_words:
+        out_file.writelines(("%s %d\n" % (words, word_freq[words]))) #writing to and formatting the output file
+
